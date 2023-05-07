fcbf218cbf633ee3a18cc1c4888abafa4ac7c557
Seungwan Woo <whoiswsw@gmail.com>
2023-05-07 11:14:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3743,36 +3743,88 @@ vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
   }, "default");
 
   // src/main.ts
+  var FLOOR_HEIGHT = 48;
+  var JUMP_FORCE = 800;
+  var SPEED = 480;
   var k = mo({
-    // width: 1024,
-    // height: 800,
-    // stretch: true,
+    // width: 640,
+    // height: 480,
+    // // stretch: false,
     // letterbox: true,
-    background: [200, 200, 200]
+    background: [200, 200, 200],
+    global: false
   });
-  k.loadSprite("wsw", "sprites/wsw.png");
-  k.add([k.text("Hello :)"), k.scale(1, 1)]);
-  var wsw = k.add([k.sprite("wsw"), k.scale(0.5, 0.5), k.pos(100, 100)]);
-  var SPEED = 200;
-  k.onKeyDown("left", () => wsw.move(-SPEED, 0));
-  k.onKeyDown("right", () => wsw.move(SPEED, 0));
-  k.onKeyDown("up", () => wsw.move(0, -SPEED));
-  k.onKeyDown("down", () => wsw.move(0, SPEED));
-  var fingerSize = 60;
-  k.onMouseMove((pos, delta) => {
-    const scale = pos.unit().x * pos.unit().y;
-    wsw.moveTo(
-      pos.x - (wsw.width / 2 + fingerSize) * scale,
-      pos.y - (wsw.height / 2 + fingerSize) * scale
-    );
-    wsw.scaleTo(scale);
+  k.loadSprite("wjh", "sprites/wjh.png");
+  k.scene("game", () => {
+    k.setGravity(2400);
+    const player = k.add([
+      k.sprite("wjh"),
+      k.scale(0.5, 0.5),
+      k.pos(320, 240),
+      k.area(),
+      k.body(),
+      k.doubleJump()
+    ]);
+    k.add([
+      k.rect(k.width(), FLOOR_HEIGHT),
+      k.outline(4),
+      k.pos(0, k.height() - FLOOR_HEIGHT),
+      k.area(),
+      k.body({ isStatic: true }),
+      k.color(127, 200, 255)
+    ]);
+    function jump() {
+      if (player.isGrounded()) {
+        player.jump(JUMP_FORCE);
+      }
+    }
+    k.onKeyPress("space", jump);
+    k.onClick(jump);
+    function spawnTree() {
+      const treeH = k.rand(32, 96);
+      k.add([
+        k.rect(48, treeH),
+        k.area(),
+        k.outline(4),
+        k.pos(k.width(), k.height() - FLOOR_HEIGHT - treeH),
+        // k.origin("botleft"),
+        k.color(255, 180, 255),
+        k.move(k.LEFT, SPEED),
+        "tree"
+      ]);
+      k.wait(k.rand(0.5, 1.5), spawnTree);
+    }
+    spawnTree();
+    let score = 0;
+    player.onCollide("tree", () => {
+      k.go("lose", score);
+      k.burp();
+      k.addKaboom(player.pos);
+    });
+    const scoreLabel = k.add([
+      k.text(score.toString()),
+      k.pos(24, 24)
+    ]);
+    k.onUpdate(() => {
+      score++;
+      scoreLabel.text = score.toString();
+    });
   });
-  k.onTouchMove((pos, t) => {
-    const scale = pos.unit().x * pos.unit().y;
-    wsw.moveTo(
-      pos.x - (wsw.width / 2 + fingerSize) * scale,
-      pos.y - (wsw.height / 2 + fingerSize) * scale
-    );
-    wsw.scaleTo(scale);
+  k.scene("lose", (score) => {
+    k.add([
+      k.sprite("wjh"),
+      k.pos(k.width() / 2, k.height() / 2 - 80),
+      k.scale(2)
+      // k.origin("center"),
+    ]);
+    k.add([
+      k.text(score),
+      k.pos(k.width() / 2, k.height() / 2 + 80),
+      k.scale(2)
+      // origin("center"),
+    ]);
+    k.onKeyPress("space", () => k.go("game"));
+    k.onClick(() => k.go("game"));
   });
+  k.go("game");
 })();
